77435207e405377bd84919f4be6cc344f03c7f4c
duffy-01 <140999121+duffy-01@users.noreply.github.com>
2024-02-18 23:34:06 -0500

    Update assignment2_q2.cpp, fixed code

--- homework2/assignment2_q2.cpp
+++ homework2/assignment2_q2.cpp
@@ -1,9 +1,10 @@
-#include <iostream> 
-#include <fstream> 
-#include <ifstream>   
+#include <iostream>
+#include <fstream>
+#include <fstream>
 #include <stdlib.h>
+#include <string>
 #include <cmath>
-#include <vector>    
+#include <vector>
 #include <numeric>
 #include <algorithm>
 
@@ -12,65 +13,63 @@ using namespace std;
 double mean_and_sdeviation(const vector<double> scores)
 {
 	double mean, standardDeviation = 0.0;
-	
+
 	int scoreSize = scores.size();
-	
+
 	cout << std::fixed;
-    	cout.precision(2);
-	
+    cout.precision(1);
+
 	//	sum elements in vector
 	auto sum = accumulate(scores.begin(), scores.end(), 0.0);
-	
+
 	mean = sum / scoreSize;
-	cout << "The average score is: " mean << endl;
-	
-	
-	for i = 0; i < scoreSize; i++)
+	cout << "The average score is: " << mean << endl;
+
+	for (int i = 0; i < scoreSize; i++)
 	{
 		standardDeviation += pow(scores[i] - mean, 2);
 	}
-	
+
 	standardDeviation = sqrt(standardDeviation/scoreSize);
 	cout << "The standard deviation is: " << standardDeviation << endl;
-	
-	
+
+
 	ofstream outputFile;
 	outputFile.open ("output.txt");
-	outputFile << "The average score is: " mean << endl;
+	outputFile << "The average score is: " << mean << endl;
 	outputFile << "The standard deviation is: " << standardDeviation << endl;
-	
+
 }
 
-int main() 
+int main()
 {
 	vector<double> scores;
 	string fileName;
 	ifstream file;
-	
+
 	cout << "Input your file name (i.e. filename.txt): ";
 	cin >> fileName;
-	
+
 	file.open(fileName);
-		
+
 	if (file.fail())
 	{
 		cout << "Error: file failed to open.\n";
 		return 1;
 	}
-	
-	
+
 	while(!file.eof())
 	{
-		getline(file, line);
+        getline(file, line);
 		scores.push_back(line);
 	}
-	
+
 	file.close();
-	
+
 	for (auto file_line : scores)
 		cout << file_line << endl;
-	
-	mean_and_sdeviation(scores)
-	
-return 0;	
+
+	mean_and_sdeviation(scores);
+
+return 0;
 }
